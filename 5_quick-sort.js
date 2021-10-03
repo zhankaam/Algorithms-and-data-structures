b6aaf14943fea66988fb23fbf500ceffc14151b7
zhankaam <zhankaam@gmail.com>
2021-10-03 12:25:09 +0300

    quick sort function

--- 5_quick-sort.js
+++ 5_quick-sort.js
@@ -4,20 +4,20 @@ let count = 0;
 
 function quickSort(array) {
   if (array.length <= 1) {
-    return array
+      return array;
 }
 let pivotIndex = Math.floor(array.length / 2);
-let pivot = array[pivotIndex]
-let less = []
-let greater = []
+    let pivot = array[pivotIndex];
+    let less = [];
+    let greater = [];
 for (let i = 0; i < array.length; i++) {
-    count += 1
-    if(i === pivotIndex)
-        continue
+    count += 1;
+    if (i === pivotIndex)
+        continue;
     if (array[i] < pivot) {
-        less.push(array[i])
+        less.push(array[i]);
     } else {
-        greater.push(array[i])
+        greater.push(array[i]);
     }
 }
 return [...quickSort(less), pivot, ...quickSort(greater)]
